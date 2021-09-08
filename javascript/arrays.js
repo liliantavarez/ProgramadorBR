d57ee9a472d5ca23691c9d65527a878c6bea6142
Lilian Carvalho <51184806+liliantavarez@users.noreply.github.com>
2021-09-08 09:04:45 -0300

    introdução sobre arrays e um pouco mais sobre o uso do for

--- javascript/arrays.js
+++ javascript/arrays.js
@@ -1,13 +1,15 @@
 var alunos = ["Igor","Luiz","Mario","Jorge"];
 
-for (const aluno of alunos) {//Pega o elemento 
+console.log(alunos)
+
+for (var aluno of alunos) {//Pega o elemento 
     console.log(aluno)    
 }
-
-for (const aluno in alunos) {//pega o indice
-    console.log(alunos[aluno]);
+console.log("")
+for (var aluno in alunos) {//pega o indice
+    console.log("Aluno "+(aluno)+": "+alunos[aluno]);
 }
-
-for (let i = 0; i < alunos.length; i++) {
-    console.log(alunos[i]);
+console.log("")
+for (var i = 0; i < alunos.length; i++) {
+    console.log("Aluno "+(i+1)+": "+alunos[i]);
 }
