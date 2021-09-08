217fa5be5eb6247b2c3dd0e48ae133e58255ff38
Lilian Carvalho <51184806+liliantavarez@users.noreply.github.com>
2021-09-08 09:29:05 -0300

    aula com os conceitos básicos sobre funções e um pouco sobre função anônima

--- javascript/funcoes.js
+++ javascript/funcoes.js
@@ -1,12 +1,21 @@
-function media(nota1,nota2) {
-    var media = (nota1+nota2)/2
-    console.log(media)
+//criando uma função de nome "media" com os argumentos "nota1 e nota2"
+function media(nota1, nota2) {
+  var media = (nota1 + nota2) / 2;
+  //console.log(media);
+  return media; //função com retorno
 }
 
-media(6,7);
-media(8,5);
+var resultado1 = media(6, 7); //chamando a função e passando os argumentos
+var m = media; //criando uma variavel e atribuindo a ela a função
 
-var media = function (n1,n2){
-    return (n1+n2)/2
-}
-console.log(media(4,5))
+var resultado2 = m(2, 5); //usando a função pela variavel
+
+console.log(resultado1);
+console.log(resultado2);
+
+//criando uma função anonima! que permite colocar uma função dentro de uma variavel
+var media = function (n1, n2) {
+  return (n1 + n2) / 2;
+};
+
+console.log(media(10, 5));
