7cdfb9a52c746735cf03b594ab0ea305898a880a
Lilian Carvalho <51184806+liliantavarez@users.noreply.github.com>
2021-09-08 11:15:39 -0300

    primeira aula sobre objetos e métodos

--- javascript/objetos.js
+++ javascript/objetos.js
@@ -2,11 +2,24 @@ function calcMedia(){
     return(this.notas[0]+this.notas[1])/2
 }
 
-var aluno = {
-    nome:"Igor",
+var escola = new Object(); //criando objeto vazio
+
+var aluno = { //criando obejto com as propriedades nome e notas
+    nome:"Igor", //chave:valor,
     notas:[5.7,8.6],
-    media: calcMedia
+    media: calcMedia //metado do objeto 
 }
 
-console.log(aluno.nome)
-console.log(aluno.media())
+var aluno2 = {
+    nome: "Jorges",
+    notas:[7.8,2.5],
+    media:calcMedia
+}
+
+aluno.matricula = 32323 //adicionando nova chave em um objeto ja criado
+aluno["sobrenome"] = "Santos" //outra maneira de adicionar nova chave
+
+console.log("Nome: "+aluno.nome)
+console.log("Media: "+aluno.media())
+console.log("Nome: "+aluno2.nome)
+console.log("Media: "+aluno2.media())
