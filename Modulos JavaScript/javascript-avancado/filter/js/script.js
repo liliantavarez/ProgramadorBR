7a01f8e011fe0d75ea49728ad78d0177e82f5b73
Lilian Carvalho <liliancarvalhotavares@gmail.com>
2021-12-13 08:37:29 -0300

    função 'filter'

--- Modulos JavaScript/javascript-avancado/filter/js/script.js
+++ Modulos JavaScript/javascript-avancado/filter/js/script.js
@@ -1,20 +1,31 @@
-function speakGeneric() {
-  console.log(this.sound);
+/*
+Filter: função do array que permite filtrar elementos de um array
+Utliza o callback para chegar um array e retornar os elementos que atendem determinada condição
+*/
+
+function novoAluno(nome, idade) {
+  return { nome, idade };
 }
 
-let dog = {
-  sound: "Au Au",
-  speak: speakGeneric,
-};
+let alunos = [
+  novoAluno("Mario", 23),
+  novoAluno("Jose", 45),
+  novoAluno("Marcia", 29),
+  novoAluno("João", 35),
+];
 
-let cat = {
-  sound: "Miau",
-  speak: speakGeneric,
-};
+console.log(alunos);
 
-dog.speak();
-cat.speak();
+let alunosComMenosDe30 = alunos.filter((aluno) => aluno.idade < 30);
+console.log(alunosComMenosDe30);
 
-let bindedFunction = speakGeneric.bind(dog);
-
-bindedFunction();
+//fazendo a função filter 
+function filtro(callback){
+  let alunosFiltrados = []
+  for (let aluno of alunos){
+    if (callback(aluno)){
+      alunosFiltrados.push(aluno)
+    }
+  }
+  return alunosFiltrados
+}
