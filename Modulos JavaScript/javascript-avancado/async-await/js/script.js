084783972fb428fdbfcdd13e392410759f7b3aed
Lilian Carvalho <liliancarvalhotavares@gmail.com>
2021-12-13 08:21:24 -0300

    aula sobre async await

--- Modulos JavaScript/javascript-avancado/async-await/js/script.js
+++ Modulos JavaScript/javascript-avancado/async-await/js/script.js
@@ -1,20 +1,31 @@
-function speakGeneric() {
-  console.log(this.sound);
+/*
+async await:
+Utilizando aync no lugar de promise 
+*/
+let usuarios = ["Luan", "Carlos", "Luana", "Maria"];
+
+let inserirUsuario = (nome) => {
+  let promise = new Promise(function (resolve, reject) {
+    setTimeout(() => {
+      usuarios.push(nome);
+      let error = false;
+
+      if (!error) {
+        resolve();
+      } else {
+        reject({ msg: "Erro de execução" });
+      }
+    }, 1000);
+  });
+  return promise;
+};
+let listarUsuarios = () => {
+  console.log(usuarios);
+};
+
+async function executar() {
+  await inserirUsuario("Marcela"); //await: faz com que o programa fique esperando ate que a promise seja executada
+  listarUsuarios(); //executa somente depois do await
 }
 
-let dog = {
-  sound: "Au Au",
-  speak: speakGeneric,
-};
-
-let cat = {
-  sound: "Miau",
-  speak: speakGeneric,
-};
-
-dog.speak();
-cat.speak();
-
-let bindedFunction = speakGeneric.bind(dog);
-
-bindedFunction();
+executar();
