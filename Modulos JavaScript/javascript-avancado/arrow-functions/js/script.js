108722efee5d03f9343b2fc0e4bcb162a4e48be5
Lilian Carvalho <liliancarvalhotavares@gmail.com>
2022-01-25 08:57:41 -0300

    this em arrows functions

--- Modulos JavaScript/javascript-avancado/arrow-functions/js/script.js
+++ Modulos JavaScript/javascript-avancado/arrow-functions/js/script.js
@@ -12,9 +12,32 @@ let dobro2 = function (x) {
 
 dobro2(5);
 
-//Array Function, com um unico argumento e somente retornado um valor
+//Array Function, com um unico argumento (uso de () tornace opcional) 
+//e somente retornado um valor (o que torna o uso de {} opcional)
 let dobro3 = x => 2 * x;
- 
+
 console.log(dobro3(6));
 
-//Array Funtion são mais simples de declarar e faceis de usar, porem ela não entede o 'this' nem o 'bind'
+//Array Funtion são mais simples de declarar e faceis de usar
+//porem ela não entede o 'this' nem o 'bind'
+
+let thisNaArrowFun = () => {console.log(this)}
+
+let thisNaFuncao = function() {
+  console.log(this)
+}
+
+let objFun = {
+  x: 8,
+  fun: thisNaFuncao
+}
+
+let objarrowFun = {
+  x: 8,
+  fun: thisNaArrowFun
+}
+
+objFun.fun() //retorna o objeto, pois este é o conteto
+objarrowFun.fun()//retorna o objeto globo como sendo o this, pq perde o contexto na arrow function 
+ 
+objarrowFun.fun = thisNaArrowFun.bind(objarrowFun)//o metado bind tambem não funciona nas arrows functions
