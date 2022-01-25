75cbc58ec86ffabcaddc0cd552c96ded3ac1ace4
Lilian Carvalho <liliancarvalhotavares@gmail.com>
2022-01-25 11:59:28 -0300

    usando o fetch para consumir uma API e constuir uma aplicação

--- Modulos JavaScript/javascript-avancado/fetch/js/script.js
+++ Modulos JavaScript/javascript-avancado/fetch/js/script.js
@@ -1,20 +1,19 @@
-function speakGeneric() {
-  console.log(this.sound);
-}
+/*pegando dados dinamicos, usando API 
+API: qualquer interface entre 2 sistemas
+*/
+let url = "https://economia.awesomeapi.com.br/last/USD-BRL";
 
-let dog = {
-  sound: "Au Au",
-  speak: speakGeneric,
+let converter = () => {
+  let valor = document.getElementById("valor").value;
+
+  fetch(url) //retorna uma promise
+  .then((res) => {
+    return res.json(); //converte para o formato JSON e retorna uma outra promise
+  })
+  .then((data) => {
+    let h1 = document.getElementById('resultado')
+    let resultado = valor * data.USDBRL.ask;
+    h1.innerHTML = `${valor} reais em dolar é: ${resultado}`
+
+    });
 };
-
-let cat = {
-  sound: "Miau",
-  speak: speakGeneric,
-};
-
-dog.speak();
-cat.speak();
-
-let bindedFunction = speakGeneric.bind(dog);
-
-bindedFunction();
