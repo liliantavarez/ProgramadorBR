15aa257de5a1f0588f581d97a3aaacd9c0393ec6
Lilian Carvalho <liliancarvalhotavares@gmail.com>
2022-01-25 10:59:45 -0300

    metado reduce

--- Modulos JavaScript/javascript-avancado/reduce/js/script.js
+++ Modulos JavaScript/javascript-avancado/reduce/js/script.js
@@ -1,20 +1,32 @@
-function speakGeneric() {
-  console.log(this.sound);
+/*
+reduce: função que pega todo os valores de um array e retorna somente um resuldado
+*/
+
+function novoAluno(nome, idade) {
+  return { nome, idade };
 }
 
-let dog = {
-  sound: "Au Au",
-  speak: speakGeneric,
-};
+let alunos = [
+  novoAluno("Mario", 23),
+  novoAluno("Jose", 45),
+  novoAluno("Marcia", 29),
+  novoAluno("João", 35),
+];
 
-let cat = {
-  sound: "Miau",
-  speak: speakGeneric,
-};
+function idadeDaTurma(total, aluno) {
+  return (total += aluno.idade);
+}
 
-dog.speak();
-cat.speak();
+console.log(alunos.reduce(idadeDaTurma, 0)); //soma a idade de todos os alunos
+//função reduce recebe como argumeto a função e o valor inicial do retorno
 
-let bindedFunction = speakGeneric.bind(dog);
+console.log(
+  alunos.reduce((todosNomes, aluno) => (todosNomes += aluno.nome), "")
+);
 
-bindedFunction();
+console.log(
+  alunos.reduce((mediaIdade, aluno) => (mediaIdade += aluno.idade),0)/alunos.length
+);
+
+
+
