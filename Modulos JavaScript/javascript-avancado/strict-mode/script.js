2262755ae915ba1c4211ce55f0424eb3d72e1275
Lilian Carvalho <liliancarvalhotavares@gmail.com>
2022-01-25 08:26:21 -0300

    reforço sobre escopos de variaveis e uso do use-strict

--- Modulos JavaScript/javascript-avancado/strict-mode/script.js
+++ Modulos JavaScript/javascript-avancado/strict-mode/script.js
@@ -1,13 +1,19 @@
-"use strict" //vaz com que seja obrigatorio declarar as variaveis 
+"use-strict"; //Faz com que seja obrigatorio declarar as variaveis
 //uma variavel criada no escopo da função ela so faz parte desta função
 
-let x2 = 2 //variavel global
+var x = 7; //VAR define a variavel de escopo GLOBAL, utilizar a palavra VAR no JS é opcional
+var x2 = 2; //variavel global pode ser acessada de qualquer lugar do programa
 
-function imprimir(){
-    let x = 7; //variavel local
-    console.log(x)
+console.log(x);
+
+function imprimir() {
+  var xFun = 8; //Toda variavel local pode ser acessada apenas no escopo que ela foi criada, nesse caso xFun só pode ser acessada na função
+  console.log(xFun);
+  y = 10; //Se o tipo de variavel não for definada o JS cria ela de forma global no objeto Windows
+  console.log(x2); //acessando variavel global dentro da função
+  console.log(y)
 }
 
-imprimir() 
-console.log(x2)
-console.log(window)
+imprimir();
+console.log(xFun); //Não é possivel acessar essa variavel pois ela foi criada dentro da função e esse é seu escopo
+console.log(y); //passa a ser inacessivel com o uso do "use strict"
