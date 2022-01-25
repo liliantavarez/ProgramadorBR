9b03286a770948ae9790f90c638294b28dcb46ea
Lilian Carvalho <liliancarvalhotavares@gmail.com>
2022-01-25 09:27:14 -0300

    uso de promise

--- Modulos JavaScript/javascript-avancado/promisses/js/script.js
+++ Modulos JavaScript/javascript-avancado/promisses/js/script.js
@@ -1,29 +1,32 @@
 /*
-Promise: Um objeto que recebe como argumento uma função()
+Promise: Um objeto que recebe como argumento uma função(), uma maneira mais nova e melhor de usar o callback
 */
 let usuarios = ["Luan", "Carlos", "Luana", "Maria"];
 
-let  inserirUsuario = (nome) => {
+let inserirUsuario = (nome) => {
   let promise = new Promise(function (resolve, reject) {
+    //criando promise, é um objeto que recebe como argumento uma função com os argumentos "resolve" e "reject"
     setTimeout(() => {
-      usuarios.push(nome);
-      let error = false;
+      usuarios.push(nome); //insere o usuario
+      let error = true; //variavel para verificar a existencia de algum erro
 
       if (!error) {
-        resolve();
+        resolve(); //a função só sera executada se não ouver nenhum erro
       } else {
-        reject({ msg: "Erro de execução" });
+        reject({ msg: "Erro na inserção de usuario" });
       }
     }, 1000);
   });
   return promise;
-}
+};
+
 let listarUsuarios = () => {
   console.log(usuarios);
-}
+};
 
 inserirUsuario("Carmem")
-  .then(listarUsuarios)//só executa quando der resolve(quando a função for executada)
-  .catch((error) => { //função que exibe o erro caso aconteca 
+  .then(listarUsuarios) //a função do .then() só é executa quando der resolve(quando a função for executada)
+  .catch((error) => {
+    //função que exibe o erro caso aconteca
     console.log(error.msg);
   });
