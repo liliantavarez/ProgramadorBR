82041a4c45e665810059d53292baa840ed3ebc94
Lilian Carvalho <liliancarvalhotavares@gmail.com>
2022-01-25 09:15:45 -0300

    uso de funçoes com callback

--- Modulos JavaScript/javascript-avancado/callback/js/script.js
+++ Modulos JavaScript/javascript-avancado/callback/js/script.js
@@ -1,35 +1,43 @@
 //Passando função como argumento de uma função
 
 function ola() {
-  console.log("Ola");
+  console.log("Olá");
 }
 
-function saudacao(s,nome){
-  s();
-  console.log(nome)
+function tchau() {
+  console.log("Tchau");
 }
 
-saudacao(ola,"Lilian")
+function saudacao(s, nome) {
+  //a função recebera como atributo a função de saudação e um nome
+  s(); //executando a função que foi recebida
+  console.log(nome);
+}
 
-let usuarios = ["Luan","Carlos","Luana","Maria"]
+saudacao(ola, "Lilian"); //passando como argumento da função, a função ola() e o nome
+saudacao(tchau, "Lilian"); //passando como argumento da função, a função tchau() e o nome
 
-function inserirUsuario(nome,callback){
+let usuarios = ["Adriano", "Marcia", "Jose"];
+
+function inserirUsuario(nome) {
+  usuarios.push(nome);
+}
+
+function listarUsuarios() {
+  console.log(usuarios);
+}
+
+inserirUsuario("Lilian");
+listarUsuarios();
+
+//Usando a função de listar usuarios com CallBack 
+
+function inserirUsuarioComCallBack(nome,callback){//recebe como argumento o nome que sera inserido e a função de listar os nomes 
   setTimeout(()=>{
-    usuarios.push(nome)
-    callback()
-  },1000)
-  
+    usuarios.push(nome)//o nome sera inserido 
+    callback()//a função que de lista os nomes sera executada somente depois que o usuario for inserido com o callback de 1s
+  },1000)//defenindo o tempo de espera 
+
 }
 
-function listarUsuarios(){
-  console.log(usuarios)
-}
-
-inserirUsuario("Carmem")
-listarUsuarios()
-
-// let dobro = function (n) {
-//   return 2 * n;
-// };
-
-// console.log(dobro(6));
+inserirUsuarioComCallBack("Laura",listarUsuarios)
