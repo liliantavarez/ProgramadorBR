fea50d32a98055fcd27e42302161496bf52dce85
Lilian Carvalho <liliancarvalhotavares@gmail.com>
2022-03-02 11:53:51 -0300

    permissoes e regras

--- Modulo - Firebase (Firestore)/permissoesUsuarios/js/permissoes.js
+++ Modulo - Firebase (Firestore)/permissoesUsuarios/js/permissoes.js
@@ -1,4 +1,4 @@
-const config  = {
+const config = {
   apiKey: "AIzaSyBpDxbHkDr4BlIUMiRo9KtHfqyTjiCSZjM",
   authDomain: "teste-96a32.firebaseapp.com",
   projectId: "teste-96a32",
@@ -10,11 +10,60 @@ const config  = {
 firebase.initializeApp(config);
 
 let db = firebase.firestore();
+let auth = firebase.auth();
 
-db.collection('lista').get().then((snapshot =>{
-    snapshot.forEach(item => {
-        console.log(item.data())
+function ler() {
+  db.collection("lista")
+    .get()
+    .then((snapshot) => {
+      snapshot.forEach((item) => {
+        console.log(item.data());
+      });
+    })
+    .catch((error) => {
+      console.log(error);
     });
-})).catch(error=>{
-    console.log(error)
-})
+}
+
+
+function escrever() {
+    db.collection("lista")
+    .add({
+        novoObjeto: "Novo Objeto",
+      numero: Math.random(),
+    })
+    .then((doc) => {
+      console.log(doc);
+    })
+    .catch((error) => {
+        console.log(error);
+    });
+}
+
+function login() {
+    let userEmail = "novoteste@teste.com";
+    let userSenha = "123abc";
+  
+    //setPersitence: Metado utilizado para alterar o estado de persistencia
+    //SESSION: So funciona para aba/janela que o login for feito
+    //LOCAL: Padrão, persiste em qualquer janela 
+    //NONE: o login é desfeito sempre que a janela for atualizada
+  
+    auth.setPersistence(firebase.auth.Auth.Persistence.SESSION).then(()=>{
+      auth.signInWithEmailAndPassword(userEmail, userSenha)
+          .then((loggedUser) => {
+            console.log(auth.currentUser);
+            escrever()
+          })
+          .catch((error) => {
+            console.log(error);
+          });
+    }).catch((error)=>{
+      console.log(error)
+    })  
+}
+
+login();
+
+// ler();
+
