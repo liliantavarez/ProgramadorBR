23c37a5453405025b5ab567e3317a89ed5363bbf
Lilian Carvalho <liliancarvalhotavares@gmail.com>
2022-05-09 11:34:08 -0300

    Selecionado elementos filhos com as funções find e children do jQuery

---
+++ Módulo - jQuery/elementosFilhos.js
@@ -0,0 +1,8 @@
+let lista1 = $("#lista1");
+
+lista1.find(".item2").hide(); //Busca todos os elementos de determinado seletor
+
+console.log(lista1.children().fadeOut(3000)) //Retorna todos os filhos
+
+let body = $("body")
+console.log(body.children("ul"))
