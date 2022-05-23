bc7d29bd7e7f8b3e6a2060527047840fc2e9966c
Lilian Carvalho <liliancarvalhotavares@gmail.com>
2022-05-23 08:42:51 -0300

    encadeamento de funções com jQuery

---
+++ Módulo - jQuery/encadeamento-chaining/script.js
@@ -0,0 +1,11 @@
+//Encademanto de funçoes 
+
+let jq = $("#lista1").slideUp(2000).slideDown(2000).fadeOut(1000);
+
+console.log($())
+console.log(jq);
+
+
+
+// let js = document.getElementById("lista1");
+// console.log(js);
