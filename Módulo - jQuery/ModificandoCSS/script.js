734d5a5e888d316e28ea9a81c5aed3fccdcb906f
Lilian Carvalho <liliancarvalhotavares@gmail.com>
2022-05-23 10:29:52 -0300

    Update script.js

--- Módulo - jQuery/ModificandoCSS/script.js
+++ Módulo - jQuery/ModificandoCSS/script.js
@@ -1,23 +1,20 @@
+setTimeout(() => {
+  $("h1").addClass("bg-blue");
+}, 1000);
 
-// setTimeout(()=>{
-//     $("h1").addClass("bg-blue")
-// },1000)
+setTimeout(() => {
+  $("h1").removeClass("bg-blue");
+}, 3000);
 
-// setTimeout(()=>{
-//     $("h1").removeClass("bg-blue")
-// },3000)
+setTimeout(() => {
+  $("h1").toggleClass("bg-blue");
+}, 3000);
 
-// setTimeout(()=>{
-//     $("h1").toggleClass("bg-blue")
-// },3000)
+setInterval(() => {
+  $("h1").toggleClass("bg-blue");
+}, 200);
 
-setInterval(()=>{
-    $("h1").toggleClass("bg-blue")
-},200)
+console.log($("h1").css("background-color"));
+console.log($("h1").css("font-size"));
 
-console.log($("h1").css("background-color"))
-console.log($("h1").css("font-size"))
-
-
-//  $("h1").css({"font-size":"4vh",
-//  "background-color":"red"})
+$("h1").css({ "font-size": "4vh", "background-color": "red" });
