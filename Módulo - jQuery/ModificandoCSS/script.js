765fe942a012e0fd68519638f99453ef8bb79eac
Lilian Carvalho <liliancarvalhotavares@gmail.com>
2022-05-23 10:28:41 -0300

    Acessando e alterando valores do CSS

---
+++ Módulo - jQuery/ModificandoCSS/script.js
@@ -0,0 +1,23 @@
+
+// setTimeout(()=>{
+//     $("h1").addClass("bg-blue")
+// },1000)
+
+// setTimeout(()=>{
+//     $("h1").removeClass("bg-blue")
+// },3000)
+
+// setTimeout(()=>{
+//     $("h1").toggleClass("bg-blue")
+// },3000)
+
+setInterval(()=>{
+    $("h1").toggleClass("bg-blue")
+},200)
+
+console.log($("h1").css("background-color"))
+console.log($("h1").css("font-size"))
+
+
+//  $("h1").css({"font-size":"4vh",
+//  "background-color":"red"})
