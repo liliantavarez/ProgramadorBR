6214b0acc90fc3eb3c0d6304f725fc8e9a0b0999
Lilian Carvalho <liliancarvalhotavares@gmail.com>
2022-05-23 08:20:50 -0300

    Update elementosFilhos.js

--- Módulo - jQuery/elementosFilhos.js
+++ Módulo - jQuery/elementosFilhos.js
@@ -2,7 +2,7 @@ let lista1 = $("#lista1");
 
 lista1.find(".item2").hide(); //Busca todos os elementos de determinado seletor
 
-console.log(lista1.children().fadeOut(3000)) //Retorna todos os filhos
+lista1.children().fadeOut(3000); //Retorna todos os filhos
 
-let body = $("body")
-console.log(body.children("ul"))
+let body = $("body");
+console.log(body.children("ul"));
