2e15e7cff169ac3230e01d4a328b9841c30e5ea7
Lilian Carvalho <51184806+liliantavarez@users.noreply.github.com>
2022-06-30 12:28:37 -0300

    tratando requests vindas do node js

--- Módulo - https/servidor-paginas/index.js
+++ Módulo - https/servidor-paginas/index.js
@@ -2,26 +2,43 @@ const http = require("http");
 const url = require("url");
 const fs = require("fs");
 
+function handleFile(req, res, callback) {
+  let path = url.parse(req.url).pathname;
+
+  if (path == "" || path == "/") {
+    path = "/index.html";
+  }
+
+  let fileName = "." + path;
+
+  fs.readFile(fileName, (err, data) => {
+    if (err) {
+        if(callback){
+            if (!callback(req,res))
+            res.writeHead(404, { "Content-Type": "text/html;charset=utf-8" });
+            res.end("<h1>Pagina não encontrada!</h1>");
+        }
+    } else {
+      res.writeHead(200, { "Content-Type": "text/html" });
+      res.write(data);
+      res.end();
+    }
+  });
+}
+
+function handleRequest(req, res) {
+  let path = url.parse(req.url).pathname;
+
+  if (path == "/teste") {
+    res.end("Teste");
+    return true
+  }
+  return false
+}
+
 http
   .createServer((request, response) => {
-    let path = url.parse(request.url).pathname;
-
-    if (path == "" || path == "/") {
-      path = "/index.html";
-    }
-
-    let fileName = "." + path;
-
-    fs.readFile(fileName, (err, data) => {
-      if (err) {
-        response.writeHead(404, { "Content-Type": "text/html;charset=utf-8" });
-        response.end("<h1>Pagina não encontrada!</h1>");
-      } else {
-        response.writeHead(200, { "Content-Type": "text/html" });
-        response.write(data);
-        response.end();
-      }
-    });
+    handleFile(request, response, handleRequest);
   })
   .listen(3000, (err) => {
     if (err) {
