3abaa39c73538f57041ed2035c3d4a13611dc6ea
Lilian Carvalho <51184806+liliantavarez@users.noreply.github.com>
2022-07-04 11:46:57 -0300

    Update index.js

--- Módulo - Node Express/instalacao/index.js
+++ Módulo - Node Express/instalacao/index.js
@@ -1,16 +1,16 @@
 /*
 Iniciar projeto: npm init -y
 Instalar express: npm install express  
-*/ 
+*/
 
 // Importando o express
 const express = require("express");
 // Importando modulo que permite montar o caminho dos arquivos
-const path = require('path')
+const path = require("path");
 //Recebendo o objeto express
 const app = express();
-//Definindo em qual diretorio estão os arquivos estaticos (HTML) 
-app.use(express.static(path.join(__dirname,'client')))
+//Definindo em qual diretorio estão os arquivos estaticos (HTML)
+app.use(express.static(path.join(__dirname, "client")));
 //Definindo porta que sera usada
 const PORT = 5000;
 
