247e1bb4e7e7000b33f26266302a4f5482c7042a
Lilian Carvalho <51184806+liliantavarez@users.noreply.github.com>
2022-08-02 10:43:56 -0300

    Update app.js

--- Módulo - MongoDb - Parte 2 (Mongoose)/app.js
+++ Módulo - MongoDb - Parte 2 (Mongoose)/app.js
@@ -2,43 +2,18 @@ const express = require("express");
 const app = express();
 const port = 3000;
 const mongoose = require("mongoose");
+
+/* Conexão com o banco de dados */
 mongoose.connect("mongodb://localhost/links");
 
-//Criando schema da coleção
-const linkSchema = new mongoose.Schema({
-  title: { type: String, required: true },
-  description: String,
-  url: { type: String, required: true },
-  click: { type: Number, default: 0 },
-});
-
-//Criando modelo baseado no schema
-const Link = mongoose.model("Link", linkSchema);
-
-//Criando novo documento que sera inserido na coleção
-let link = new Link({
-  title: "Twitter",
-  description: "Link para o twitter",
-  url: "https://twitter.com/liliantavarez",
-});
-
 let db = mongoose.connection;
+
 db.on("error", () => {
   console.log("Houve um erro!");
 });
 
 db.once("open", () => {
   console.log("Banco Carregado..");
-  //   console.log(db);
-  //Salvando no DB
-  link
-    .save()
-    .then((doc) => {
-      console.log("Documento inserido com sucesso!", doc);
-    })
-    .catch((err) => {
-      console.log("Erro ao inserir documento na coleção!", err);
-    });
 });
 
 app.get("/", (req, res) => res.send("Hello Word"));
@@ -46,3 +21,28 @@ app.get("/", (req, res) => res.send("Hello Word"));
 app.listen(port, () => {
   console.log("Servidor rodando....");
 });
+
+/* Criando schema da coleção */
+const linkSchema = new mongoose.Schema({
+  title: { type: String, required: true },
+  description: String,
+  url: { type: String, required: true },
+  click: { type: Number, default: 0 },
+});
+
+/* Criando modelo baseado no schema */
+const Link = mongoose.model("Link", linkSchema);
+
+/* Criando novo documento que sera inserido na coleção */ 
+let link = new Link({
+  title: "Twitter",
+  description: "Link para o twitter",
+  url: "https://twitter.com/liliantavarez",
+});
+
+/* Salvando no DB */
+link.save().then((doc) => {
+    console.log("Documento inserido com sucesso!", doc);
+  }).catch((err) => {
+    console.log("Erro ao inserir documento na coleção!", err);
+  });
