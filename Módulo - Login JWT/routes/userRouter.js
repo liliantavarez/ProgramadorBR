b86836d24415b0584476a4d8c81341ea5dcf32fc
Lilian Carvalho <51184806+liliantavarez@users.noreply.github.com>
2022-08-16 09:21:23 -0300

    configurando conexão com banco de dados

--- Módulo - Login JWT/routes/userRouter.js
+++ Módulo - Login JWT/routes/userRouter.js
@@ -1,6 +1,16 @@
 const express = require("express");
 const router = express.Router();
 const userController = require("../controllers/userController");
+const mongoose = require("mongoose");
+
+mongoose.connect(process.env.MONGOOSE_URL_CONNECTION, (error)=>{
+    if(error){
+        console.log(error)
+    }else{
+        console.log("Mongoose connected")
+    }
+})
+
 
 router.post("/register", userController.register);
 
